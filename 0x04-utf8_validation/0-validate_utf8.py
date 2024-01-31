b461ff415c772f148c2532a245480f2030aaedc3
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2024-01-31 20:40:34 +0000

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Check if the given data set represents a valid UTF-8 encoding.
+"""
+
 
 def validUTF8(data):
     """Check if the given data set represents a valid UTF-8 encoding.
